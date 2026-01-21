dab187f826107d0803662147479037f8d9335c87
curiel <curiel.connect@gmail.com>
2026-01-20 19:47:17 -0600

    print welcome message

--- ito.c
+++ ito.c
@@ -9,6 +9,8 @@
 #include <unistd.h>
 
 /*** defines ***/
+#define ITO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
@@ -129,7 +131,26 @@ void editorDrawRows(struct abuf *ab) {
 	int y;
 
 	for (y = 0; y < E.screenrows; y++) {
-        abAppend(ab, "~", 1);
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                                      "Ito editor -- version %s", ITO_VERSION);
+            if (welcomelen > E.screencols)
+                welcomelen = E.screencols;
+            
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding > 0) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding-- > 0)
+                abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+        }
+
+        abAppend(ab, "\x1b[K", 3);
 		if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
 		}
@@ -139,12 +160,13 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen(void) {
     struct abuf ab = ABUF_INIT;
 
-    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[?25l", 6);
     abAppend(&ab, "\x1b[H", 3);
 
 	editorDrawRows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 3);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
